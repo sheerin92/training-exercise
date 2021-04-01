915fe7c9d478d33f076dfdeedfdfff378b3c15d6
sheerinumar <sheerinsidhara@gmail.com>
2021-04-01 11:52:00 +0530

    canvas_task

--- simple_exercises/myBind.js
+++ simple_exercises/myBind.js
@@ -1,7 +1,8 @@
 let employee = {
     name: "Sheerin",
-    getName: function(){
-        return this.name;
+    getName: function(city,companyName,designation){
+        return `Hi ${this.name}, Welcome to ${city}. You are currently an employee of ${companyName}
+        and your designation is ${designation}`;
     }
 }
 let employee1 = {
@@ -9,10 +10,14 @@ let employee1 = {
 }
 
 function myBind(empObj,getNameFun){
+    var args = [];
+    for(var i = 2; i < arguments.length; i++ ){
+        args.push(arguments[i]);
+    }
     return function(){
-       return getNameFun.apply(empObj);
+       return getNameFun.apply(empObj,args);
     }
 }
-var myBindRes = myBind(employee, employee.getName);
-//var myBindRes = myBind(employee1, employee.getName);
+var myBindRes = myBind(employee, employee.getName,'Chennai','Provility','Software Trainee');
+//var myBindRes = myBind(employee1, employee.getName,'Chennai','Provility','Software Trainee');
 console.log(myBindRes());
